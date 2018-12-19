ed026ad723a2d0e8958f2359cb4bee3915c32027
ssd <512552874@qq.com>
2018-12-19 15:21:17 +0800

    Done

--- app.py
+++ app.py
@@ -1,6 +1,29 @@
-from api import app
-from models import db
+from flask import Flask
+from config import db, photo_test
+from apis import api
+from flask_migrate import Migrate
+from flask_uploads import configure_uploads, IMAGES
+import os
 
-# db.init_app(app)
-if __name__ == "__main__":
-    app.run(debug = True)
+def create_app():
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:123456@127.0.0.1:3306/richText'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+    app.config['UPLOADED_PHOTO_DEST'] = os.path.dirname(os.path.abspath(__file__)) + '/file_uploads'
+    app.config['UPLOADED_PHOTO_ALLOW'] = IMAGES
+    api.init_app(app)
+    db.init_app(app)
+    configure_uploads(app, photo_test)
+    @app.after_request
+    def after_request(response):
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+        response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+        return response
+    migrate = Migrate(app, db)
+    return app
+
+app = create_app()
+
+if __name__ == '__main__':
+    app.run()
--- models.py
+++ models.py
@@ -1,13 +1,7 @@
-from  flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+
 from datetime import datetime
-import  pymysql
+from config import db
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:123456@127.0.0.1:3306/richText'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-
-db = SQLAlchemy(app)
 
 class Content(db.Model):
     __tableName__ = "content"
@@ -17,6 +11,3 @@ class Content(db.Model):
 
     def __repr__(self):
         return '<Content %r>' % self.id
-
-if __name__ == "__main__":
-    db.create_all()
