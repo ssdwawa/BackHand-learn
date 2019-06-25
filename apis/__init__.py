4287f935b1fadeb0512ed7e16073951dbb8af10e
Song Shida <512552874@qq.com>
2019-06-25 17:59:37 +0800

    Update __init__.py

--- apis/__init__.py
+++ apis/__init__.py
@@ -1,7 +1,8 @@
 from flask_restplus import Api
 from .textInput import api as text
+from .imgshow import api as img
 
 api = Api(doc='/doc')
 api.add_namespace(text, path='/text')
-
+api.add_namespace(img, path='/img')
 
